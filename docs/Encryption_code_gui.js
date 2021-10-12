bb31874a420d02509386ee4e48cd898c3d0f1bef
Shubham <71990977+Skparab1@users.noreply.github.com>
2021-10-12 10:07:18 -0700

    added alternative string slice for Mac+safari bug fix

--- docs/Encryption_code_gui.js
+++ docs/Encryption_code_gui.js
@@ -1851,7 +1851,7 @@ if (keyCode == BACKSPACE || keyCode == DELETE){
   typed = typed.substring(0, typed.length -1);
   }
   
-  username = username.replace('Backspac','');
+  username = username.substring(0, username.length -7);
   
   if (keyCode == ENTER){
   if (accountclick == 'none' && display == 'account'){
